72b5e3ed77e38303be84e02cc1f62a4b2d97ff5a
McGroove <nikola.vilar@student.um.si>
2021-01-22 20:45:38 +0100

    updates

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -31,12 +31,12 @@ module.exports = {
             }
 
             var resl = [];
-            
-            for(var i in patients) {
+
+            for (var i in patients) {
                 var newPat = patients[i].toObject();
-                
+
                 const key = EC.keyFromPrivate(newPat.private_key, 'hex');
-        
+
                 newPat.public_key = key.getPublic().encode('hex', false);
 
                 resl.push(newPat);
@@ -51,7 +51,7 @@ module.exports = {
      */
     show: function (req, res) {
         var id = req.params.id;
-        patientModel.findOne({_id: id}, function (err, patient) {
+        patientModel.findOne({ _id: id }, function (err, patient) {
             if (err) {
                 return res.status(500).json({
                     message: 'Error when getting patient.',
@@ -95,25 +95,25 @@ module.exports = {
         });*/
         //var test = connectedModel.sendHttpPost("/wallet/newWallet", null);
 
-        const sendData = JSON.stringify({"data": null});
+        const sendData = JSON.stringify({ "data": null });
 
-		
-		const options = {
-			hostname: global.host_ip,
-			port: global.host_port_int,
-			path: "/wallet/newWallet",
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'Content-Length': sendData.length
-			}
-		};
-		
-		const reqs = http.request(options, resl => {
+
+        const options = {
+            hostname: global.host_ip,
+            port: global.host_port_int,
+            path: "/wallet/newWallet",
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': sendData.length
+            }
+        };
+
+        const reqs = http.request(options, resl => {
             //console.log("statusCode: " + res.statusCode + ";");
             let data = '';
 
-			resl.on('data', d => {
+            resl.on('data', d => {
                 //process.stdout.write(d)
                 //console.log(JSON.stringify(d, null, "\t"));
 
@@ -121,7 +121,7 @@ module.exports = {
 
                 data += d;
             });
-            
+
             resl.on('end', () => {
 
                 //console.log(data);
@@ -129,21 +129,21 @@ module.exports = {
                 var medical_number_value = req.body.medical_number;
                 var full_name_value = req.body.full_name;
 
-                if(medical_number_value == null)
+                if (medical_number_value == null)
                     medical_number_value = req.body.medicalNumber;
 
-                if(full_name_value == null)
+                if (full_name_value == null)
                     full_name_value = req.body.fullName;
 
                 var patient = new patientModel({
-                    medical_number : medical_number_value,
-                    password : req.body.password,
-                    full_name : full_name_value,
-                    phone : req.body.phone,
-                    location : req.body.location,
+                    medical_number: medical_number_value,
+                    password: req.body.password,
+                    full_name: full_name_value,
+                    phone: req.body.phone,
+                    location: req.body.location,
                     private_key: JSON.parse(data).privateKey
                 });
-        
+
                 patient.save(function (err, patient) {
                     if (err) {
                         return res.status(500).json({
@@ -152,42 +152,42 @@ module.exports = {
                         });
                     }
                     return res.status(201).json({
-                        _id : patient._id,
-                        id : patient._id,
-                        medical_number : patient.medical_number,
-                        medicalNumber : patient.medical_number,
-                        password : patient.password,
-                        full_name : patient.full_name,
-                        fullName : patient.full_name,
-                        phone : patient.phone,
-                        location : patient.location,
+                        _id: patient._id,
+                        id: patient._id,
+                        medical_number: patient.medical_number,
+                        medicalNumber: patient.medical_number,
+                        password: patient.password,
+                        full_name: patient.full_name,
+                        fullName: patient.full_name,
+                        phone: patient.phone,
+                        location: patient.location,
                         privateKey: patient.private_key,
                         private_key: patient.private_key
                     });
                 });
             });
-		});
+        });
 
-		reqs.on('error', error => {
-			//pomeni, da tista adresa ni dosegljiva --> brisemo je iz seznama
-			/*connected.removeNode(this.address.toString(), this.port);
-			console.log("[[in post]]removed from connection with port: " + this.port.toString());
-			
-			if(ip.address().toString() === saServerIp && parseInt(serverPortNumber) === saServerPort && connected.getConnectedNodesLength() <= 1) {	//samo server je v p2p omrezju
-				writeToFile();
-			}
-		
-			//posiljanje spremembe vsemi ostalimi vozlisci
-			connected.sendHttpPostToAll(ip.address().toString(), parseInt(serverPortNumber), "/nodes/nodeNetworkChanged", JSON.stringify(connected));
-			*/
-			
+        reqs.on('error', error => {
+            //pomeni, da tista adresa ni dosegljiva --> brisemo je iz seznama
+            /*connected.removeNode(this.address.toString(), this.port);
+            console.log("[[in post]]removed from connection with port: " + this.port.toString());
+        	
+            if(ip.address().toString() === saServerIp && parseInt(serverPortNumber) === saServerPort && connected.getConnectedNodesLength() <= 1) {	//samo server je v p2p omrezju
+                writeToFile();
+            }
+    	
+            //posiljanje spremembe vsemi ostalimi vozlisci
+            connected.sendHttpPostToAll(ip.address().toString(), parseInt(serverPortNumber), "/nodes/nodeNetworkChanged", JSON.stringify(connected));
+            */
+
             //console.error(error);
-            
+
             console.log("error: " + JSON.stringify(error));
-		});
-	
-		reqs.write(sendData);
-		reqs.end();
+        });
+
+        reqs.write(sendData);
+        reqs.end();
     },
 
     /**
@@ -195,7 +195,7 @@ module.exports = {
      */
     update: function (req, res) {
         var id = req.params.id;
-        patientModel.findOne({_id: id}, function (err, patient) {
+        patientModel.findOne({ _id: id }, function (err, patient) {
             if (err) {
                 return res.status(500).json({
                     message: 'Error when getting patient',
@@ -208,13 +208,20 @@ module.exports = {
                 });
             }
 
-            patient.medical_number = req.body.medical_number ? req.body.medical_number : patient.medical_number;
-            patient.password = req.body.password ? req.body.password : patient.password;
+            if (req.body.password != null) {
+                patient.password = req.body.password;
+                patient.changed = true;
+            }
+            else {
+                patient.changed = false;
+            }
+               
             patient.full_name = req.body.full_name ? req.body.full_name : patient.full_name;
-			patient.phone = req.body.phone ? req.body.phone : patient.phone;
+            patient.full_name = req.body.fullName ? req.body.fullName : patient.full_name;
+            patient.phone = req.body.phone ? req.body.phone : patient.phone;
             patient.location = req.body.location ? req.body.location : patient.location;
-           
-			
+
+
             patient.save(function (err, patient) {
                 if (err) {
                     return res.status(500).json({
@@ -223,7 +230,25 @@ module.exports = {
                     });
                 }
 
-                return res.json(patient);
+                var newPat = patient.toObject();
+                const key = EC.keyFromPrivate(newPat.private_key, 'hex');
+                newPat.public_key = key.getPublic().encode('hex', false);
+
+                return res.status(201).json({
+                    _id: patient._id,
+                    id: patient._id,
+                    medical_number: patient.medical_number,
+                    medicalNumber: patient.medical_number,
+                    password: patient.password,
+                    full_name: patient.full_name,
+                    fullName: patient.full_name,
+                    phone: patient.phone,
+                    location: patient.location,
+                    privateKey: patient.private_key,
+                    private_key: patient.private_key,
+                    public_key: newPat.public_key,
+                    publicKey: newPat.public_key
+                });
             });
         });
     },
@@ -244,50 +269,50 @@ module.exports = {
         });
     },
 
-    login: function (req, res,next) {
-       
+    login: function (req, res, next) {
+
         var medical_number_value = req.body.medical_number;
-        if(medical_number_value == null)
-                    medical_number_value = req.body.medicalNumber;
+        if (medical_number_value == null)
+            medical_number_value = req.body.medicalNumber;
 
         patientModel.authenticate(medical_number_value, req.body.password, function (error, patient) {
-        if (error || !patient) {
-          var err = new Error('Wrong medical number or password.');
-          err.status = 401;
-          return next(err);
-        } else {
-          req.session.patientId = patient._id;
+            if (error || !patient) {
+                var err = new Error('Wrong medical number or password.');
+                err.status = 401;
+                return next(err);
+            } else {
+                req.session.patientId = patient._id;
 
-          const key = EC.keyFromPrivate(patient.private_key, 'hex');
+                const key = EC.keyFromPrivate(patient.private_key, 'hex');
 
-           return res.status(201).json({
-            _id : patient._id,
-            id : patient._id,
-            medical_number : patient.medical_number,
-            medicalNumber : patient.medical_number,
-            password : patient.password,
-            full_name : patient.full_name,
-            fullName : patient.full_name,
-            phone : patient.phone,
-            location : patient.location,
-            privateKey: patient.private_key,
-            private_key: patient.private_key,
-            publicKey : key.getPublic().encode('hex', false),
-            public_key : key.getPublic().encode('hex', false)
-        });
-        }
-      })
+                return res.status(201).json({
+                    _id: patient._id,
+                    id: patient._id,
+                    medical_number: patient.medical_number,
+                    medicalNumber: patient.medical_number,
+                    password: patient.password,
+                    full_name: patient.full_name,
+                    fullName: patient.full_name,
+                    phone: patient.phone,
+                    location: patient.location,
+                    privateKey: patient.private_key,
+                    private_key: patient.private_key,
+                    publicKey: key.getPublic().encode('hex', false),
+                    public_key: key.getPublic().encode('hex', false)
+                });
+            }
+        })
     },
 
-    logout: function (req, res,next) {
+    logout: function (req, res, next) {
         if (req.session) {
-     req.session.destroy(function (err) {
-       if (err) {
-         return next(err);
-       } else {
-         return res.status(200);
-       }
-     });
-     }
-   }
+            req.session.destroy(function (err) {
+                if (err) {
+                    return next(err);
+                } else {
+                    return res.status(200);
+                }
+            });
+        }
+    }
 };
--- models/patientModel.js
+++ models/patientModel.js
@@ -1,48 +1,51 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
-var Schema   = mongoose.Schema;
+var Schema = mongoose.Schema;
 
 var patientSchema = new Schema({
-	'medical_number' : String,
-	'password' : String,
-	'full_name' : String,
-	'phone' : String,
-	'location' : String,
+	'medical_number': String,
+	'password': String,
+	'full_name': String,
+	'phone': String,
+	'location': String,
 	'private_key': String
 });
 
 
 patientSchema.statics.authenticate = function (medical_number, password, callback) {
 	Patient.findOne({ medical_number: medical_number })
-	  .exec(function (err, patient) {
-		if (err) {
-		  return callback(err)
-		} else if (!patient) {
-		  var err = new Error('Patient not found.');
-		  err.status = 401;
-		  return callback(err);
-		}
-		bcrypt.compare(password, patient.password, function (err, result) {
-		  if (result === true) {
-			return callback(null, patient);
-		  } else {
-			return callback();
-		  }
-		})
-	  });
-  }
-  
-  patientSchema.pre('save', function (next) {
-	var patient = this;
-	bcrypt.hash(patient.password, 10, function (err, hash) {
-	  if (err) {
-		return next(err);
-	  }
-	  patient.password = hash;
-	  next();
-	})
-  });
+		.exec(function (err, patient) {
+			if (err) {
+				return callback(err)
+			} else if (!patient) {
+				var err = new Error('Patient not found.');
+				err.status = 401;
+				return callback(err);
+			}
+			bcrypt.compare(password, patient.password, function (err, result) {
+				if (result === true) {
+					return callback(null, patient);
+				} else {
+					return callback();
+				}
+			})
+		});
+}
 
-  var Patient = mongoose.model('Patient', patientSchema);
-  module.exports = Patient;
+patientSchema.pre('save', function (next) {
+	var patient = this;
+	if (patient.changed) {
+		bcrypt.hash(patient.password, 10, function (err, hash) {
+			if (err) {
+				return next(err);
+			}
+			patient.password = hash;
+			next();
+		})
+	} else
+		next();
+});
+
+var Patient = mongoose.model('Patient', patientSchema);
+module.exports = Patient;
 
